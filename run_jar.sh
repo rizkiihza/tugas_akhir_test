791dba3f38c2465bf4840f3df54d1f15809c3093
rizkiihza <rizki.ihza@gmail.com>
2019-04-13 13:23:09 +0700

    add data, django web, contoh, and tictactoe

--- run_jar.sh
+++ run_jar.sh
@@ -1,2 +1,2 @@
-java -jar program.jar -r output/data_text/data_$1.txt output/data_predicate/predicate_dict_$1.json
+java -jar program.jar -r output/data_text/data_$1.txt output/data_predicate/predicate_dict_$1.json 5000 2
 
